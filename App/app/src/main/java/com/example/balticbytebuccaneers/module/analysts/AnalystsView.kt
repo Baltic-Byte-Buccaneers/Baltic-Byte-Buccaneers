cd4e1581ed4213193b69e5535d96e181ddbeb55a
Waringh4m <82370122+Waringh4m@users.noreply.github.com>
2023-10-20 22:40:47 +0200

    analysis WIP

--- App/app/src/main/java/com/example/balticbytebuccaneers/module/analysts/AnalystsView.kt
+++ App/app/src/main/java/com/example/balticbytebuccaneers/module/analysts/AnalystsView.kt
@@ -1,2 +1,109 @@
 package com.example.balticbytebuccaneers.module.analysts
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.balticbytebuccaneers.R
+
+@Composable
+fun AnalystsView(){
+    Column() {
+        PaginationView()
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PaginationView() {
+    // Display 10 items
+    val pageCount = 10
+    val pagerState = rememberPagerState(pageCount = {
+        10
+    })
+    Column (modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text(
+            text = "Analysis",
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.onBackground,
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold
+        )
+
+        HorizontalPager(
+            state = pagerState,
+            pageSize = PageSize.Fill,
+            modifier = Modifier.weight(1.0F)) { page ->
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Analysis $page",
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
+        }
+        Row(
+            Modifier
+                .height(50.dp)
+                .fillMaxWidth(),
+                //.align(Alignment.BottomCenter),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pageCount) { iteration ->
+                val color =
+                    if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(20.dp)
+
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PaginationPreview() {
+    PaginationView()
+}
