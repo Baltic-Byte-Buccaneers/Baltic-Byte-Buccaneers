00478bd992c946b71ead18d58421d0d306bf17b7
Kevin <87549467+sakecoincognito@users.noreply.github.com>
2023-10-20 17:15:23 +0200

    #4 Set proper theme colors, auto formatting

--- App/app/src/main/java/com/example/balticbytebuccaneers/module/receiptList/ReceiptListViewItem.kt
+++ App/app/src/main/java/com/example/balticbytebuccaneers/module/receiptList/ReceiptListViewItem.kt
@@ -2,12 +2,15 @@ package com.example.balticbytebuccaneers.module.receiptList
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ReceiptLong
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,23 +21,28 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ReceiptListViewItem(receipt: Receipt) {
-    Row(modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment =Alignment.CenterVertically
-    ) {
-        Icon(
-            imageVector= Icons.Filled.ReceiptLong,
-            contentDescription = "",
-            modifier =
-            Modifier.size(64.dp).background(Color.LightGray)
-        )
-        Text(text=receipt.description, color= Color.White)
-        Text(text=receipt.amount, color=Color.Gray)
+    Box(modifier = Modifier.background(MaterialTheme.colorScheme.secondaryContainer)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Icon(
+                imageVector = Icons.Filled.ReceiptLong,
+                contentDescription = "",
+                modifier =
+                Modifier.size(64.dp)
+            )
+
+            Text(text = receipt.description, color = MaterialTheme.colorScheme.onSecondaryContainer)
+            Text(text = receipt.amount, color = MaterialTheme.colorScheme.onSecondaryContainer)
+        }
     }
 }
 
 @Preview
 @Composable
 fun ReceiptListViewItemPreview() {
-    ReceiptListViewItem(Receipt("Wasserflasche", "12.43 €"))
+    ReceiptListViewItem(Receipt("Edeka Sunwold", "12.43 €"))
 }
