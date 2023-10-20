8992f7d54ae75ea5602dd25a8ff6b655fa901a61
Kevin <87549467+sakecoincognito@users.noreply.github.com>
2023-10-20 14:16:36 +0200

    #4 Added dummy list

--- App/app/src/main/java/com/example/balticbytebuccaneers/module/home/HomeView.kt
+++ App/app/src/main/java/com/example/balticbytebuccaneers/module/home/HomeView.kt
@@ -1,2 +1,3 @@
 package com.example.balticbytebuccaneers.module.home
 
+ReceiptListView()
--- App/app/src/main/java/com/example/balticbytebuccaneers/module/receiptList/ReceiptListView.kt
+++ App/app/src/main/java/com/example/balticbytebuccaneers/module/receiptList/ReceiptListView.kt
@@ -2,5 +2,20 @@ package com.example.balticbytebuccaneers.module.receiptList
 
 @Composable
 fun ReceiptListView() {
+    LazyColumn {
+        // Add a single item
+        item {
+            Text(text = "Kondome - 5€")
+        }
 
+        // Add 5 items
+        items(5) { index ->
+            Text(text = "Wasserflasche: $index - 1,40 €")
+        }
+
+        // Add another single item
+        item {
+            Text(text = "Klopapier - 45 €")
+        }
+    }
 }
