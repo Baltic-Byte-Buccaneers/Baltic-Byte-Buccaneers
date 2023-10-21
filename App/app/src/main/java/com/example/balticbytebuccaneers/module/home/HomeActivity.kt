fc8d1490be5db7102502b0439d89045e776d9ae5
lukasSch98 <schueler98@gmail.com>
2023-10-21 16:49:50 +0200

    QS

--- App/app/src/main/java/com/example/balticbytebuccaneers/module/home/HomeActivity.kt
+++ App/app/src/main/java/com/example/balticbytebuccaneers/module/home/HomeActivity.kt
@@ -24,14 +24,7 @@ class HomeActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //HomeView()
-                    val context = LocalContext.current
-                    val viewModel = remember {
-                        ReceiptDetailViewModel("") {
-                            Toast.makeText(context, "Back pressed", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                    ReceiptDetailView(viewModel = viewModel)
+                    HomeView()
                 }
             }
         }
