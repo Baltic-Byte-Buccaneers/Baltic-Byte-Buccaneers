f4e036885f349af5b34fb908137c2bb224528fbf
matthias <git@melwert.de>
2023-10-21 15:33:12 +0200

    add receipt entry category

--- backend/api/receiptEntry.go
+++ backend/api/receiptEntry.go
@@ -4,6 +4,7 @@ type ReceiptEntry struct {
 	Title         string  `bson:"title" json:"title"`
 	Quantity      int     `bson:"quanitiy" json:"quantity"`
 	Amount        float64 `bson:"amount" json:"amount"`
+	Category      float64 `bson:"category" json:"category"`
 	VatRate       float32 `bson:"vatRate" json:"vatRate,omitempty"`
 	ProducerId    string  `bson:"producerId" json:"producerId,omitempty"`
 	PriceTendency string  `bson:"priceTendency" json:"priceTendency"`
