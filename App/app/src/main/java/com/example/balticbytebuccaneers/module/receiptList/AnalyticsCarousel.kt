0a25bfc7e25f0ec1ed3626bb31b53980f947fb09
Kevin <kevin.schwedersky@sakeco.de>
2023-10-21 16:19:59 +0200

    #4 added pagination indicator

--- App/app/src/main/java/com/example/balticbytebuccaneers/module/receiptList/AnalyticsCarousel.kt
+++ App/app/src/main/java/com/example/balticbytebuccaneers/module/receiptList/AnalyticsCarousel.kt
@@ -1,15 +1,24 @@
 package com.example.balticbytebuccaneers.module.receiptList
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,6 +76,25 @@ fun AnalyticsCarousel() {
                     vendorChartData,
                     pieChartConfig
                 )
+        }
+        Row(
+            Modifier
+                .height(50.dp)
+                .fillMaxWidth()
+                .align(CenterHorizontally),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(20.dp)
+
+                )
+            }
         }
     }
 }
